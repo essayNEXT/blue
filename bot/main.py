a996d624f620f3cf782074262b55d608f6111eac
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-02-21 12:13:31 +0200

    add router to main.py and change main finction

--- bot/main.py
+++ bot/main.py
@@ -1,22 +1,19 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
 from aiogram.types import Message
 from settings import BOT_TOKEN
 from aiogram.filters import Command
 
-
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
-dp = Dispatcher()
+router = Router()
 
 
-@dp.message(Command(commands=["start"]))
+@router.message(Command(commands=["start"]))
 async def cmd_start(message: Message):
     await message.answer("Hello!")
 
 
-@dp.message()
+@router.message()
 async def cmd_start(message: Message):
     await message.answer(f"Від користувача - {message.from_user.id}\n"
                          f"{message.text}")
@@ -24,6 +21,10 @@ async def cmd_start(message: Message):
 
 # Запуск бота
 async def main():
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
+    dp = Dispatcher()
+    dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
