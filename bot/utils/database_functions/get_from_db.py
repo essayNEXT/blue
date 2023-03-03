5336a48950bcddaf0f8d87bd4819ecfcc5e4ee50
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-03-03 16:43:00 +0200

    Описано функції, що імітують роботу бази даних

--- bot/utils/database_functions/get_from_db.py
+++ bot/utils/database_functions/get_from_db.py
@@ -1,10 +1,17 @@
 # тут має бути імпорт модуля роботи з БД
 
-def is_in_database(user_id: int) -> bool:
+exist_id = []
+
+
+def user_is_in_database(user_id: int) -> bool:
     """Функція порівнює вказаний user_id з наявними в базі даних.
     У разі наявності такого в БД повератає True"""
     # 386532326
-    exist_id = []  # тут має бути використано запит до БД на витяг всіх зареєстрованих id в базі даних
+    # exist_id = []  # тут має бути використано запит до БД на витяг всіх зареєстрованих id в базі даних
+    global exist_id
     return user_id in exist_id
 
 
+async def add_guest_to_db(user_id: int):
+    global exist_id
+    exist_id.append(user_id)
