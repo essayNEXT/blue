feacd3122898c8032df1b61c226cd2c81319b49f
Onyshchenko Serhii <19taurus79@gmail.com>
2023-03-09 12:22:05 +0200

    Додав функцію запису до БД

--- bot/handlers/echo.py
+++ bot/handlers/echo.py
@@ -1,9 +1,14 @@
 from aiogram import Router
 from aiogram.types import Message
+from utils.database_functions.create_table import execut_query
 
 router = Router()
 
 
 @router.message()
 async def echo(message: Message):
+    insert_query = (f"INSERT INTO message (message, userid) "
+                    f"VALUES ({message.text},{message.from_user.id})")
+    execut_query(insert_query)
+
     await message.answer(message.text)
---
+++ bot/utils/database_functions/create_table.py
@@ -0,0 +1,20 @@
+from .connection import con
+from psycopg2 import OperationalError
+
+
+def execut_query(query, connection=con):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print('Query executed successfully')
+    except OperationalError as e:
+        print(f'Error{e}')
+
+
+create_message_table = """
+CREATE TABLE IF NOT EXISTS message(
+message TEXT,
+userid INTEGER)"""
+
+execut_query(create_message_table)
