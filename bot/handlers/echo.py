e6b7978ec935cb4198a4adbc438d6630e3d24469
Onyshchenko Serhii <19taurus79@gmail.com>
2023-03-10 14:07:56 +0200

    Повернув файл .env.example

--- bot/handlers/echo.py
+++ bot/handlers/echo.py
@@ -1,9 +1,18 @@
 from aiogram import Router
+from aiogram.filters import Command
 from aiogram.types import Message
 from utils.database_functions.create_table import execut_query
 from datetime import datetime
 
 router = Router()
+
+
+@router.message(Command('read_last'))
+async def cmd_read_last(message: Message):
+    insert_query = "SELECT message_time FROM message ORDER BY message_time DESC LIMIT 1"
+    val = execut_query(insert_query)
+    print(insert_query)
+    await message.answer(f'Дата последнего сообщения  в БД {val}')
 
 
 @router.message()
