fe215b6cc5c80d631c5429b756cef48330349337
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-03-12 14:23:09 +0200

    working scrolling inline keyboard

--- bot/handlers/echo.py
+++ bot/handlers/echo.py
@@ -1,18 +1,18 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
-#from utils.database_functions.create_table import execute_query
+from utils.database_functions.create_table import execute_query
 from datetime import datetime
-#from utils.database_functions.connection import con
+from utils.database_functions.connection import con
 
 router = Router()
 
 
 @router.message(Command('read_last'))
 async def cmd_read_last(message: Message):
-    #cursor = con.cursor()
-    #cursor.execute("SELECT message FROM message ORDER BY message_time DESC LIMIT 1")
-    #val = cursor.fetchone()
+    cursor = con.cursor()
+    cursor.execute("SELECT message FROM message ORDER BY message_time DESC LIMIT 1")
+    val = cursor.fetchone()
     await message.answer(f'Последнее сообщение в БД "{val[0]}"')
 
 
@@ -21,6 +21,6 @@ async def echo(message: Message):
     date = datetime.now().isoformat(" ")
     insert_query = (f"INSERT INTO message (message, userid, message_time) "
                     f"VALUES ('{message.text}','{message.from_user.id}','{date}')")
-    #execute_query(insert_query)
+    execute_query(insert_query)
 
     await message.answer(message.text)
--- bot/handlers/inline_kb.py
+++ bot/handlers/inline_kb.py
@@ -1,19 +1,23 @@
 from aiogram import Router
 from aiogram.utils.keyboard import InlineKeyboardButton
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.filters import Command, Text
+from aiogram.types import Message, CallbackQuery
 from keyboards.inline import CombineInlineKeyboardGenerator
-from aiogram.fsm.state import State
-from
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
+
+class InlineStates(StatesGroup):
+    Inline = State()
 
 
 router = Router()
 
 
 @router.message(Command(commands='inline_kb'))
-async def get_inline_kb(event: Message):
-    num_of_scrolls = 8
+async def get_inline_kb(event: Message, state: FSMContext):
+    await state.set_state(InlineStates.Inline)
+    num_of_scrolls = 20
     scroll_key_buttons = [
         [
             InlineKeyboardButton(
@@ -43,5 +47,29 @@ async def get_inline_kb(event: Message):
         max_rows_number=5,
         scroll_step=1
     )
+    kb_data = {"keyboard": kb}
+
+    await state.set_data(kb_data)
+
     await event.answer("Це твоя інлайн клавіатура",
                        reply_markup=kb.markup())
+
+
+@router.callback_query(Text(text="keyboard_down"))
+async def down_button(event: CallbackQuery, state: FSMContext):
+    kb_data = await state.get_data()
+    kb = kb_data['keyboard']
+    await event.message.edit_text("Клавіатура після кнопки вниз",
+                                  reply_markup=kb.markup_down())
+    kb_data = {"keyboard": kb}
+    await state.set_data(kb_data)
+
+
+@router.callback_query(Text(text="keyboard_up"))
+async def down_button(event: CallbackQuery, state: FSMContext):
+    kb_data = await state.get_data()
+    kb = kb_data['keyboard']
+    await event.message.edit_text("Клавіатура після кнопки вверх",
+                                  reply_markup=kb.markup_up())
+    kb_data = {"keyboard": kb}
+    await state.set_data(kb_data)
