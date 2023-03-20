22c5ce3f1591df8418d1d369c916f3ae74161f9d
Onyshchenko Serhii <19taurus79@gmail.com>
2023-03-20 15:51:47 +0200

    Оновив налаштування DATABASE в файлі settings.py (Django)

--- django_blue/admin_bot/settings.py
+++ django_blue/admin_bot/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'admin_bot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('POSTGRES_DB', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('POSTGRES_USER', 'user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
