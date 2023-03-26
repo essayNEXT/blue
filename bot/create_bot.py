d51ef034d1459638e4898f76e35754ed23f85258
Onyshchenko Serhii <19taurus79@gmail.com>
2023-03-26 10:21:30 +0300

    Виправив помилки

--- bot/create_bot.py
+++ bot/create_bot.py
@@ -1,12 +1,20 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from utils.storages import TmpStorage
+from redis.asyncio.client import Redis
+import os
 
+from utils.storages import TmpStorage
+from aiogram.fsm.storage.redis import RedisStorage
 
 from settings import BOT_TOKEN
 
+redis = Redis(
+        port=os.environ.get('REDIS_PORT'),
+        host=os.environ.get('REDIS_HOST')
+    )
 
-storage = MemoryStorage()
 tmp_storage = TmpStorage()
+storage = RedisStorage(redis=redis)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(storage=storage, tmp_storage=tmp_storage)
+
--- bot/main_bot.py
+++ bot/main_bot.py
@@ -9,18 +9,19 @@ import logging
 from handlers import echo, inline_kb
 from utils.commands import set_commands
 from aiogram.fsm.storage.redis import RedisStorage
+from create_bot import bot, dp
 
 
 async def main():
     """ Функція запуску бота"""
     logging.basicConfig(level=logging.INFO)
-    bot = Bot(token=BOT_TOKEN)
+    # bot = Bot(token=BOT_TOKEN)
     await set_commands(bot)
-    redis = Redis(
-        port=os.environ.get('REDIS_PORT'),
-        host=os.environ.get('REDIS_HOST')
-    )
-    dp = Dispatcher(storage=RedisStorage(redis=redis))
+    # redis = Redis(
+    #     port=os.environ.get('REDIS_PORT'),
+    #     host=os.environ.get('REDIS_HOST')
+    # )
+    # dp = Dispatcher(storage=RedisStorage(redis=redis))
     dp.include_router(inline_kb.router)
     dp.include_router(echo.router)
     await bot.delete_webhook(drop_pending_updates=True)
