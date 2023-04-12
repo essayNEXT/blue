6a79c04efe7045d35d985e9103c351fd79bdf28d
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-04-12 22:39:44 +0300

    Оновив хендлери для створення клавіатури

--- bot/handlers/inline_kb.py
+++ bot/handlers/inline_kb.py
@@ -3,7 +3,7 @@ from aiogram import Router
 from aiogram.filters import Command, Text
 from aiogram.types import Message, CallbackQuery
 from keyboards.inline import KeyKeyboard
-from keyboards.custom_inline_keyboard import MyCustomKeyboard
+from keyboards.custom_inline_keyboard import MyCustomKeyboard, MyCustomKeyboard2
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from utils.storages import TmpStorage
@@ -37,6 +37,8 @@ async def get_test_kb(event: Union[Message, CallbackQuery], state: FSMContext, t
             message_id=event.message_id
         )
         tmp_storage[key] = kb
+        print(id(kb))
+        # del i
 
         await event.answer(kb.text, reply_markup=kb.markup())
 
@@ -48,11 +50,44 @@ async def get_test_kb(event: Union[Message, CallbackQuery], state: FSMContext, t
             message_id=event.message.message_id - 1
         )
         kb = tmp_storage[key]
-        if event.data == "#_test_scroll_up":
-            kb.markup_up()
-        elif event.data == "#_test_scroll_down":
-            kb.markup_down()
-        else:
-            kb.callback(event)
+
+        kb.callback(event)
+
+        await event.message.edit_text(kb.text, reply_markup=kb.markup())
+
+
+@router.message(Command(commands='test2_kb'))
+@router.callback_query(Text(startswith="#_test2_"))
+async def get_test_kb(event: Union[Message, CallbackQuery], state: FSMContext, tmp_storage: TmpStorage):
+    """Хендлер для тестової клавіатури MyCustomKeyboard"""
+    if isinstance(event, Message):
+        await state.set_state(InlineStates.Inline)
+
+        user_language = event.from_user.language_code
+        user_id = event.from_user.id
+        # user_language = "uk"
+
+        kb = MyCustomKeyboard2(user_language=user_language, user_id=user_id, dp=router)
+
+        key = KeyKeyboard(
+            bot_id=bot.id,
+            chat_id=event.chat.id,
+            user_id=event.from_user.id if event.from_user else None,
+            message_id=event.message_id
+        )
+        tmp_storage[key] = kb
+
+        await event.answer(kb.text, reply_markup=kb.markup())
+
+    if isinstance(event, CallbackQuery):
+        key = KeyKeyboard(
+            bot_id=bot.id,
+            chat_id=event.message.chat.id,
+            user_id=event.from_user.id if event.from_user else None,
+            message_id=event.message.message_id - 1
+        )
+        kb = tmp_storage[key]
+
+        kb.callback(event)
 
         await event.message.edit_text(kb.text, reply_markup=kb.markup())
